4baa2f6a2c0ce98f7f8e73e1f6635b72fdea5b52
CautiLas <lautarocastillo2703@gmail.com>
2025-12-07 20:10:59 -0300

    jeje

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int numero = 6;
-        System.out.println(Factorial.factorialMethod(numero));
-        System.out.println(Fibonacci.fibonacciMethod(numero));
-        System.out.println(MaxArray.calcMaxArray());
+        System.out.println(Factorial.factorialMethod(5));
+        System.out.println(Fibonacci.fibonacciMethod(7));
+        System.out.println(MaxArray.calcMaxNumber());
         System.out.println(Palindrome.isPalindrome("reconocer"));
         System.out.println(LongestString.findLongest());
     }
--- src/MaxArray.java
+++ src/MaxArray.java
@@ -3,7 +3,7 @@ public class MaxArray {
     static int[] numbers = {1, 2, 3, 4, 5, 6, 7};
     static int maxNumber = numbers[0];
 
-   public static int calcMaxArray() {
+   public static int calcMaxNumber() {
         for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] > maxNumber) {
                 maxNumber = numbers[i];
